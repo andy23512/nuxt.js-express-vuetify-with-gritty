6777df3c2a0437727197e7903fb0a7bd6d6c7f5e
andy23512 <andy23512@gmail.com>
2018-08-08 01:51:33 +0800

    add gritty page

---
+++ pages/gritty/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="container">
+    <div class="gritty"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  mounted () {
+    if (process.browser) {
+      const gritty = require('gritty/dist-dev/gritty.js')
+      debugger  // eslint-disable-line
+      const prefix = '/gritty' // default
+      const env = {} // default
+      const fontFamily = 'Courier' // default
+
+      gritty('.gritty', {
+        prefix,
+        env,
+        fontFamily
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.gritty {
+  height: 100%;
+}
+</style>
+
